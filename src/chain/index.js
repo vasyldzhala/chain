5901c0ae440684d5d5a1f04cf9c11087556408ca
vasyldzhala <v.barbarossa@gmail.com>
2018-11-16 15:28:08 +0200

    'fix_const'

--- src/chain/index.js
+++ src/chain/index.js
@@ -3,7 +3,9 @@ import 'isomorphic-fetch';
 export const chain = async arr => {
   const rez = [];
   for (let i = 0; i < arr.length; i++) {
-    await fetch(arr[i]).then(res => res.json()).then(r => rez.push(r));
+    await fetch(arr[i])
+      .then(resp => resp.json())
+      .then(resp => rez.push(resp));
   }
   return rez;
 };
